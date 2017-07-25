cd2421d1c691ddb07a8adf3b9cd6a46d0b557b0e
Emile <emilisbliudzius@gmail.com>
2017-07-25 12:07:34 +0200

    trying some different libs

--- play.go
+++ play.go
@@ -1,30 +1,95 @@
 package ep
 
 import (
-	"os/exec"
+	"errors"
+	"github.com/hajimehoshi/oto"
+	"github.com/tcolgate/mp3"
 	"net/http"
 	"strings"
-	"errors"
+	"sync"
+)
+
+const (
+	bufferSize = 0x2000
 )
 
 func Stream(episode Episode) error {
-	if !strings.HasSuffix(episode.URL, ".mp3") {
-		return errors.New("currently only mp3 is supported")
+	cleanURL := strings.Trim(episode.URL, " ")
+	if !strings.HasSuffix(cleanURL, ".mp3") {
+		return errors.New("Currently only mp3 files are supported")
 	}
-	resp, err := http.Get(strings.Trim(episode.URL, " "))
+
+	resp, err := http.Get(cleanURL)
 	if err != nil {
 		return err
 	}
-	dataChan := make(chan []byte, 64)
 
-	cmd := exec.Command("mpg123", "-q", "-")
+	var frame mp3.Frame
+	var skipped int
+	dec := mp3.NewDecoder(resp.Body)
+	/* Decode one frame */
+	err = dec.Decode(&frame, &skipped)
+	header := frame.Header()
 
-	cmd.Stdin = resp.Body
-	err = cmd.Start()
+	player, err := oto.NewPlayer(int(header.SampleRate()), 1, 1, bufferSize)
 	if err != nil {
-		close(dataChan)
 		return err
 	}
 
-	return cmd.Wait()
+	beginStream(dec, player).Wait()
+	return nil
+}
+
+func beginStream(decoder *mp3.Decoder, player *oto.Player) *sync.WaitGroup {
+	defer player.Close()
+
+	bufChan := make(chan []uint8, 64)
+	defer close(bufChan)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	go parseMp3(decoder, bufChan, wg)
+	go writeToStream(player, bufChan, wg)
+
+	return wg
+}
+
+func parseMp3(decoder *mp3.Decoder, buffChan chan []uint8, wg *sync.WaitGroup) {
+	frame := mp3.Frame{}
+	var skipped int
+	for skipped != 0 {
+		decoder.Decode(&frame, &skipped)
+
+		reader := frame.Reader()
+		buf := make([]uint8, bufferSize)
+		var count int = -1
+		var err error
+
+		/* Write whole frame in bufferSize chunks */
+		for count != 0 {
+			count, err = reader.Read(buf)
+			if err != nil {
+				panic("stream read error: " + err.Error())
+			}
+			buffChan <- buf
+		}
+	}
+
+	/* Finish waitgroup */
+	wg.Add(-1)
+}
+
+func writeToStream(player *oto.Player, buffChan chan []uint8, wg *sync.WaitGroup) {
+	var count int = -1
+	var err error
+	for count != 0 {
+		count, err = player.Write(<-buffChan)
+		if err != nil {
+			panic("player error: " + err.Error())
+		}
+	}
+
+	/* Finish waitgroup */
+	wg.Add(-1)
 }

8a8ea1c2c785d1406115ea04e83a0550cdbd4977
Emile <emilisbliudzius@gmail.com>
2017-07-27 19:40:13 +0200

    added wav playback

--- play.go
+++ play.go
@@ -2,10 +2,13 @@ package ep
 
 import (
 	"errors"
-	"github.com/hajimehoshi/oto"
-	"github.com/tcolgate/mp3"
-	"net/http"
 	"strings"
+	"net/http"
+	"github.com/faiface/beep/wav"
+	"github.com/faiface/beep"
+	"io"
+	"github.com/faiface/beep/speaker"
+	"time"
 	"sync"
 )
 
@@ -15,8 +18,14 @@ const (
 
 func Stream(episode Episode) error {
 	cleanURL := strings.Trim(episode.URL, " ")
-	if !strings.HasSuffix(cleanURL, ".mp3") {
-		return errors.New("Currently only mp3 files are supported")
+	ext, err := getExtension(cleanURL)
+	if err != nil {
+		return err
+	}
+
+	decodeFunc, ok := audioDecoders[ext]
+	if !ok {
+		return errors.New("There is no handler for ." + ext + " files")
 	}
 
 	resp, err := http.Get(cleanURL)
@@ -24,72 +33,40 @@ func Stream(episode Episode) error {
 		return err
 	}
 
-	var frame mp3.Frame
-	var skipped int
-	dec := mp3.NewDecoder(resp.Body)
-	/* Decode one frame */
-	err = dec.Decode(&frame, &skipped)
-	header := frame.Header()
-
-	player, err := oto.NewPlayer(int(header.SampleRate()), 1, 1, bufferSize)
+	stream, format, err := decodeFunc(resp.Body)
 	if err != nil {
 		return err
 	}
 
-	beginStream(dec, player).Wait()
+	/* Start audio */
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	/* Create a waitgroup for this thread */
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	callback := func() {
+		wg.Add(-1)
+	}
+
+	speaker.Play(beep.Seq(stream, beep.Callback(callback)))
+	wg.Wait()
+
 	return nil
 }
 
-func beginStream(decoder *mp3.Decoder, player *oto.Player) *sync.WaitGroup {
-	defer player.Close()
-
-	bufChan := make(chan []uint8, 64)
-	defer close(bufChan)
-
-	wg := &sync.WaitGroup{}
-	wg.Add(2)
-
-	go parseMp3(decoder, bufChan, wg)
-	go writeToStream(player, bufChan, wg)
-
-	return wg
+func getExtension(url string) (string, error) {
+	sections := strings.Split(url, "/")
+	if len(sections) == 0 {
+		return "", errors.New("URL has no sections")
+	}
+	dotted := strings.Split(sections[len(sections) - 1], ".")
+	if len(dotted) == 0 {
+		return "", errors.New("No extension can be found")
+	}
+	return dotted[len(dotted) - 1], nil
 }
 
-func parseMp3(decoder *mp3.Decoder, buffChan chan []uint8, wg *sync.WaitGroup) {
-	frame := mp3.Frame{}
-	var skipped int
-	for skipped != 0 {
-		decoder.Decode(&frame, &skipped)
-
-		reader := frame.Reader()
-		buf := make([]uint8, bufferSize)
-		var count int = -1
-		var err error
-
-		/* Write whole frame in bufferSize chunks */
-		for count != 0 {
-			count, err = reader.Read(buf)
-			if err != nil {
-				panic("stream read error: " + err.Error())
-			}
-			buffChan <- buf
-		}
-	}
-
-	/* Finish waitgroup */
-	wg.Add(-1)
-}
-
-func writeToStream(player *oto.Player, buffChan chan []uint8, wg *sync.WaitGroup) {
-	var count int = -1
-	var err error
-	for count != 0 {
-		count, err = player.Write(<-buffChan)
-		if err != nil {
-			panic("player error: " + err.Error())
-		}
-	}
-
-	/* Finish waitgroup */
-	wg.Add(-1)
+var audioDecoders = map[string]func(io.ReadCloser) (beep.StreamSeekCloser, beep.Format, error) {
+	"wav":wav.Decode,
 }
